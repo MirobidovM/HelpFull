a005c678b854cef2d20aca53dec460c4dbf0399d
MirobidovM <miriliyosmiriobidov@gmail.com>
2022-06-07 14:12:53 +0500

    end

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(function(){
+   $('.kurs-slider, .slider__items').slick({
+       arrows: false,
+       dots:true,
+       fade:true,
+       autoplay: true,
+       autoplaySpeed: 2000,
+   });
+});
